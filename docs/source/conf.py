5864d0c9e88ba6d7c868d116fefc6322759f6848
Eugene Yang <eugene87222@gmail.com>
2022-11-02 00:24:06 +0800

    update files for readthedocds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,13 +22,18 @@ extensions = [
    'sphinx.ext.doctest',
    'sphinx.ext.autodoc',
    'sphinx.ext.autosummary',
+   'sphinx.ext.intersphinx',
    # 'autoapi.extension',
 ]
 
-templates_path = ['_templates']
-exclude_patterns = []
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
-master_doc = 'index'
+templates_path = ['_templates']
+# exclude_patterns = []
 
 # autoapi_dirs = ['../../src']
 
