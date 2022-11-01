4599426ba8bebe9ef1689beeaca0f9641da4417c
Eugene Yang <eugene87222@gmail.com>
2022-11-02 00:18:46 +0800

    update files for readthedocds

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,4 +36,4 @@ master_doc = 'index'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_static_path = ['_static']
