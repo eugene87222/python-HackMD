a99915f13ca89021c2ddc15dc19568ccdc0a27ac
Eugene Yang <eugene87222@gmail.com>
2022-11-02 23:15:42 +0800

    update files for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@ sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'src')))
 project = 'python-HackMD'
 copyright = '2022, Eugene Yang'
 author = 'Eugene Yang'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
