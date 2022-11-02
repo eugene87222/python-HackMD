d1be03e6cd02335aeb188f7a561153cdcd790c2a
Eugene Yang <eugene87222@gmail.com>
2022-11-02 23:23:14 +0800

    update files for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,10 +19,7 @@ release = '1.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-   'sphinx.ext.duration',
-   'sphinx.ext.doctest',
    'sphinx.ext.autodoc',
-   'sphinx.ext.autosummary',
 ]
 
 templates_path = ['_templates']
